904b5cc4aaee5f5b07189aa7b5d02540b5d6e1d8
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-17 22:21:23 -0400

    circle.js and user input

--- index.js
+++ index.js
@@ -3,3 +3,32 @@ const fs = require('fs');
 const { Square } = require('./lib/square.js');
 const { Circle } = require('./lib/circle.js');
 const { Triangle } = require('./lib/triangle.js');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'input three characters for the logo.',
+            name: 'characters',
+        },
+        {
+            type: 'input',
+            message: 'input character color text.',
+            name: 'textColor',
+        },
+        {
+            type: 'rawlist',
+            message: 'use arrow keys to select a shape for your logo.',
+            choices: [
+                { name: 'Square', value: 'Square' },
+                { name: 'Circle', value: 'Circle' },
+                { name: 'Triangle', value: 'Triangle' },
+            ],
+            name: 'shape',
+        },
+        {
+            type: 'input',
+            message: 'Enter shapes color.',
+            name: 'shapeColor',
+        },
+    ])
