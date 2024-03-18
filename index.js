0ad0a6be4d0f849fbedabfc44d83270d94f2dcae
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-17 22:07:16 -0400

    stater files

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Square } = require('./lib/square.js');
+const { Circle } = require('./lib/circle.js');
+const { Triangle } = require('./lib/triangle.js');
